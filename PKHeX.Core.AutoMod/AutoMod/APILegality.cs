74b9d16fad0f9cc734b6b14926af23780623b8bb
William Jackson <will-jj@users.noreply.github.com>
2025-02-02 22:53:30 +0000

    WIP Legality continued

--- PKHeX.Core.AutoMod/AutoMod/APILegality.cs
+++ PKHeX.Core.AutoMod/AutoMod/APILegality.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using static PKHeX.Core.AutoMod.TracebackType;
+
 namespace PKHeX.Core.AutoMod
 {
     /// <summary>
@@ -899,8 +900,7 @@ namespace PKHeX.Core.AutoMod
             var ivprop = enc.GetType().GetProperty("IVs");
             if (enc is not EncounterStatic4Pokewalker && enc.Generation > 2)
                 ShowdownEdits.SetNature(pk, set, enc);
-                // TODO: See why ivprop isn't working
-            pk.SetIVs(set.IVs);
+
             // If PID and IV is handled in PreSetPIDIV, don't set it here again and return out
             var hascurry = set.GetBatchValue(nameof(IRibbonSetMark8.RibbonMarkCurry));
             var changeec =
